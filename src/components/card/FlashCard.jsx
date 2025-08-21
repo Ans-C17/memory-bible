6fa93eca3270f539217cb10fa0bf66b39873db7f
Ans Christy <jovanacarmel@gmail.com>
2025-08-21 16:06:25 +0530

    outline was redundant

--- src/components/card/FlashCard.jsx
+++ src/components/card/FlashCard.jsx
@@ -19,7 +19,7 @@ function FlashCard() {
   const handleRating = () => setShowNextButton(true);
 
   return (
-    <div className="h-72 w-11/12 py-6 max-w-sm sm:h-80 sm:max-w-md md:h-96 md:max-w-lg lg:max-w-xl xl:max-w-2xl bg-blue-600 border-8 border-blue-600 outline outline-3 outline-white -outline-offset-16 flex flex-col justify-center items-center text-center">
+    <div className="h-72 w-11/12 py-6 max-w-sm sm:h-80 sm:max-w-md md:h-96 md:max-w-lg lg:max-w-xl xl:max-w-2xl bg-blue-600 border-8 border-blue-600 outline-3 outline-white -outline-offset-16 flex flex-col justify-center items-center text-center">
       <div className="flex-1 flex items-center justify-center">
         {showQuote ? (
           <FlashCardBack verse={verses[nextIndex]} />
