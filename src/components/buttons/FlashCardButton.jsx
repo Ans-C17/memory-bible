4560d0ba0724a2b9541d4216df8d085330911d5d
Ans Christy <jovanacarmel@gmail.com>
2025-09-02 23:21:14 +0530

    style: changed flashcard color to cyan-600

--- src/components/buttons/FlashCardButton.jsx
+++ src/components/buttons/FlashCardButton.jsx
@@ -1,7 +1,7 @@
 function FlashCardButton({ onClick, display }) {
   return (
     <button
-      className="bg-white text-blue-600 px-6 py-3 rounded-lg font-medium transform hover:scale-105 transition-all duration-200 shadow-md hover:shadow-lg"
+      className="bg-white text-cyan-600 px-6 py-3 rounded-lg font-medium transform hover:scale-105 transition-all duration-200 shadow-md hover:shadow-lg"
       onClick={onClick}
     >
       {display}
--- src/components/card/FlashCard.jsx
+++ src/components/card/FlashCard.jsx
@@ -21,7 +21,7 @@ function FlashCard() {
   };
 
   return (
-    <div className="h-72 w-11/12 py-6 max-w-sm sm:h-80 sm:max-w-md md:h-96 md:max-w-lg lg:max-w-xl xl:max-w-2xl bg-blue-600 border-8 border-blue-600 outline-3 outline-white -outline-offset-16 flex flex-col justify-center items-center text-center">
+    <div className="h-72 w-11/12 py-6 max-w-sm sm:h-80 sm:max-w-md md:h-96 md:max-w-lg lg:max-w-xl xl:max-w-2xl bg-cyan-600 border-8 border-cyan-600 outline-3 outline-white -outline-offset-16 flex flex-col justify-center items-center text-center">
       <div className="flex-1 flex items-center justify-center">
         {showQuote ? (
           <FlashCardBack verse={verses[nextIndex]} />
